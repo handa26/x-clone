7955a67d2bf39b8fd5b2dc65ded460893ed80183
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2026-07-08 09:06:47 +0800

    feat: responsive right bar design

--- client/src/components/RightBar.tsx
+++ client/src/components/RightBar.tsx
@@ -1,7 +1,27 @@
-const RightBar = () => {
-  return (
-    <div className=''>RightBar</div>
-  )
-}
+import Link from "next/link";
 
-export default RightBar
+import Search from "./Search";
+import PopularTags from "./PopularTags";
+import Recommendations from "./Recommendations";
+
+const RightBar = () => {
+	return (
+		<div className="pt-4 flex flex-col gap-4 sticky top-0 h-max">
+			<Search />
+			<PopularTags />
+			<Recommendations />
+
+			{/* FOOTER */}
+			<div className="text-textGray text-sm flex gap-x-4 flex-wrap">
+				<Link href="/">Terms of Service</Link>
+				<Link href="/">Privacy Policy</Link>
+				<Link href="/">Cookie Policy</Link>
+				<Link href="/">Accessibility</Link>
+				<Link href="/">Ads Info</Link>
+				<span>© 2026 L Corp.</span>
+			</div>
+		</div>
+	);
+};
+
+export default RightBar;
