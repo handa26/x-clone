20ccaaa395b6d01a16e22f5858ff0f73ce7feb0d
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2026-07-14 10:12:05 +0800

    refactor: prisma migrate, studio, initial db setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,40 @@
-import Link from "next/link"
+import Link from "next/link";
 
-import Feed from "@/components/Feed"
-import Share from "@/components/Share"
+import Feed from "@/components/Feed";
+import Share from "@/components/Share";
 
-const Homepage = () => {
-  return (
-    <div className=''>
-      <div className="px-4 pt-4 flex justify-between text-textGray font-bold border-b-[1px] border-borderGray">
-        <Link className="pb-3 flex items-center border-b-4 border-iconBlue" href="/">For you</Link>
-        <Link className="pb-3 flex items-center" href="/">Following</Link>
-        <Link className="hidden pb-3 md:flex items-center" href="/">React.js</Link>
-        <Link className="hidden pb-3 md:flex items-center" href="/">Javascript</Link>
-        <Link className="hidden pb-3 md:flex items-center" href="/">CSS</Link>
-      </div>
+import { prisma } from "@/prisma";
 
-      <Share />
-      <Feed />
-    </div>
-  )
-}
+const Homepage = async () => {
+	// const users = await prisma.user.delete({ where: { id: "test" } });
 
-export default Homepage
+	return (
+		<div className="">
+			<div className="px-4 pt-4 flex justify-between text-textGray font-bold border-b-[1px] border-borderGray">
+				<Link
+					className="pb-3 flex items-center border-b-4 border-iconBlue"
+					href="/"
+				>
+					For you
+				</Link>
+				<Link className="pb-3 flex items-center" href="/">
+					Following
+				</Link>
+				<Link className="hidden pb-3 md:flex items-center" href="/">
+					React.js
+				</Link>
+				<Link className="hidden pb-3 md:flex items-center" href="/">
+					Javascript
+				</Link>
+				<Link className="hidden pb-3 md:flex items-center" href="/">
+					CSS
+				</Link>
+			</div>
+
+			<Share />
+			<Feed />
+		</div>
+	);
+};
+
+export default Homepage;
