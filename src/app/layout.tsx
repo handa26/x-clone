3ca088c96a0605f37d7c087ec303dfd7367836ce
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2026-07-22 15:45:01 +0800

    feat: add official metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,16 @@
 import { ImageKitProvider } from "@imagekit/next";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Metadata } from "next";
 
 import QueryProvider from "@/providers/QueryProvider";
 
 import "./globals.css";
+
+export const metadata: Metadata = {
+	title: "Twitter/X",
+	description:
+		"Welcome to Twitter/X, your trusted digital town square, where conversations unfold in real time, get the full story with all the live commentary.",
+};
 
 export default function AppLayout({
 	children,
