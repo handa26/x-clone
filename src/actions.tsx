d19c5f2afc62ccef5a7cbede6afef0cc8024a25b
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2026-07-28 14:30:02 +0800

    feat: implement follow/unfollow function

--- src/actions.tsx
+++ src/actions.tsx
@@ -13,6 +13,29 @@ export const shareAction = async (formData: FormData) => {
 	console.log(file, desc);
 };
 
+export const followUser = async (targetUserId: string) => {
+	const { userId } = await auth();
+
+	if (!userId) return;
+
+	const existingFollow = await prisma.follow.findFirst({
+		where: {
+			followerId: userId,
+			followingId: targetUserId,
+		},
+	});
+
+	if (existingFollow) {
+		await prisma.follow.delete({
+			where: { id: existingFollow.id },
+		});
+	} else {
+		await prisma.follow.create({
+			data: { followerId: userId, followingId: targetUserId },
+		});
+	}
+};
+
 export const likePost = async (postId: number) => {
 	const { userId } = await auth();
 
--- src/components/FollowButton.tsx
+++ src/components/FollowButton.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { useState, useOptimistic } from "react";
+
+import { followUser } from "@/actions";
+
 const FollowButton = ({
 	userId,
 	isFollowed,
@@ -7,10 +11,26 @@ const FollowButton = ({
 	userId: string;
 	isFollowed: boolean;
 }) => {
+	const [state, setState] = useState(isFollowed);
+
+	const followAction = async () => {
+		switchOptimisticFollow("");
+		await followUser(userId);
+
+		setState((prev) => !prev);
+	};
+
+	const [optimisticFollow, switchOptimisticFollow] = useOptimistic(
+		state,
+		(prev) => !prev,
+	);
+
 	return (
-		<button className="py-2 px-4 bg-white text-black font-bold rounded-full">
-			{isFollowed ? "Unfollow" : "Follow"}
-		</button>
+		<form action={followAction}>
+			<button className="py-2 px-4 bg-white text-black font-bold rounded-full">
+				{optimisticFollow ? "Unfollow" : "Follow"}
+			</button>
+		</form>
 	);
 };
 
